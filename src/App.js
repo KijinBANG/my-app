96043c98968abbfa20ee4bfe4836c64f205d1f95
KijinBANG <mr.kijin.bang@gmail.com>
2022-04-22 11:20:55 +0900

    Study about 'ref:'

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-import { Component } from "react";
+import {Component} from "react";
 import './App.css';
 import MyComponent from "./Component";
 import Counter from "./Counter";
 import Say from "./Say";
 import EventPractice from "./EventPractice";
 import EPWithFunctionComponent from "./EPWithFunctionComponent";
+import ValidationSample from "./ValidationSample";
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
     render() {
         const name = 'react';
         return (
-            <MyComponent name={'방기진'} favoriteNumber={7}>율&연</MyComponent>,
-            <Counter />,
-            <Say />,
-            <EventPractice />,
-            <EPWithFunctionComponent />
+            <div>
+
+                <MyComponent name={'방기진'} favoriteNumber={7}>율&연</MyComponent>
+                <Counter/>
+                <Say/>
+                <EventPractice/>
+                <EPWithFunctionComponent/>
+                <ValidationSample/>
+                <ScrollBox ref={(ref) => this.ScrollBox=ref}/>
+                <button onClick={() => this.ScrollBox.scrollToBottom()}>
+                    {/* 위의 code 에서 onClick = {this.ScrollBoc.scrollToBottom} 이라 하면 오류가 발생하는 이유를 생각 해 보자. */}
+                    맨 밑으로
+                </button>
+            </div>
         )
     };
 }
