3cd49bbf733defcadfafc40ec3864285202867ca
KijinBANG <mr.kijin.bang@gmail.com>
2022-04-22 14:48:21 +0900

    Study about 'repeat Component'

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import EventPractice from "./EventPractice";
 import EPWithFunctionComponent from "./EPWithFunctionComponent";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
+import IterationSample1 from "./IterationSample1";
+import IterationSample2 from "./IterationSample2";
 
 class App extends Component {
     render() {
         const name = 'react';
         return (
             <div>
-
                 <MyComponent name={'방기진'} favoriteNumber={7}>율&연</MyComponent>
                 <Counter/>
                 <Say/>
@@ -25,6 +26,8 @@ class App extends Component {
                     {/* 위의 code 에서 onClick = {this.ScrollBoc.scrollToBottom} 이라 하면 오류가 발생하는 이유를 생각 해 보자. */}
                     맨 밑으로
                 </button>
+                <IterationSample1/>
+                <IterationSample2/>
             </div>
         )
     };
